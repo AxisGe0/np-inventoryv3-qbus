e37864e243a959ab3d7d3de2c48ab56823506019
AxisGe0 <69110100+AxisGe0@users.noreply.github.com>
2021-03-22 22:35:56 +0530

    Add files via upload

---
+++ ax-inventory/__resource.lua
@@ -0,0 +1,39 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description 'Lauda Inventory Made By Axis'--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]
+
+server_scripts {
+	"config.lua",
+	"server/main.lua",
+}
+
+client_scripts {
+	"config.lua",
+	"client/main.lua",
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/main.css',
+	'html/js/app.js',
+	'html/images/*.png',
+	'html/images/*.jpg',
+	'html/ammo_images/*.png',
+	'html/attachment_images/*.png',
+	'html/*.ttf',
+}
+--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]--[[MADE BY Ax is bro ther made by axis brother dream life roleplay ax is brother Axis#1672
+fuck Indian Empire RolePlay Fuk You All Halka gorib you too!]]
